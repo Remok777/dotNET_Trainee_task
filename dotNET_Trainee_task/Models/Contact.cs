b4609b17909b663010acb7989566b40e094682bf
Remok777 <roman.sadokha01@gmail.com>
2021-02-02 13:07:49 +0200

    Made the main model and created context model for DB

--- dotNET_Trainee_task/Models/Contact.cs
+++ dotNET_Trainee_task/Models/Contact.cs
@@ -7,5 +7,11 @@ namespace dotNET_Trainee_task.Models
 {
     public class Contact
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Maried { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
     }
 }
---
+++ dotNET_Trainee_task/Models/ContactContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNET_Trainee_task.Models
+{
+    public class ContactContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
