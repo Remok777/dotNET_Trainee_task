25adbd0e5bc0268515e5d8d004db271ddeab8b29
Remok777 <roman.sadokha01@gmail.com>
2021-02-02 10:56:00 +0200

    Add project files.

---
+++ dotNET_Trainee_task/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNET_Trainee_task.Models
+{
+    public class Contact
+    {
+    }
+}
---
+++ dotNET_Trainee_task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNET_Trainee_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
