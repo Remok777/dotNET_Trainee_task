80b3102929867ba613fd3b78412ac23c3d18c817
Remok777 <roman.sadokha01@gmail.com>
2021-02-02 23:45:40 +0200

    Connected MSSQLSERVER and added migration

--- dotNET_Trainee_task/Models/ContactContext.cs
+++ dotNET_Trainee_task/Models/ContactContext.cs
@@ -9,5 +9,9 @@ namespace dotNET_Trainee_task.Models
     public class ContactContext : DbContext
     {
         public DbSet<Contact> Contacts { get; set; }
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+
+        }
     }
 }
--- dotNET_Trainee_task/Startup.cs
+++ dotNET_Trainee_task/Startup.cs
@@ -1,6 +1,8 @@
+using dotNET_Trainee_task.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace dotNET_Trainee_task
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ContactContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
